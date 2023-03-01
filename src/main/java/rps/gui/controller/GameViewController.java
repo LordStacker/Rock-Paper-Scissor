39a8c980a7bcb9f722672031b9aa3558879401c2
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-03-01 11:40:48 +0100

    implement AI into UI

--- src/main/java/rps/gui/controller/GameViewController.java
+++ src/main/java/rps/gui/controller/GameViewController.java
@@ -1,6 +1,9 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableIntegerValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -8,6 +11,8 @@ import javafx.fxml.Initializable;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
 import io.github.palexdev.materialfx.controls.*;
@@ -15,8 +20,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import rps.Main;
-import rps.bll.game.GameManager;
-import rps.bll.game.Move;
+import rps.bll.game.*;
 import rps.bll.player.IPlayer;
 import rps.bll.player.Player;
 import rps.bll.player.PlayerType;
@@ -35,6 +39,8 @@ public class GameViewController implements Initializable {
     private IPlayer player;
     private IPlayer bot;
     private GameManager gameManager;
+    private final IntegerProperty playerScore = new SimpleIntegerProperty();
+    private final IntegerProperty botScore = new SimpleIntegerProperty();
     /**
      * Initializes the controller class.
      */
@@ -44,6 +50,11 @@ public class GameViewController implements Initializable {
         //TODO BRING THE NAME FROM FIRST FXML
         player = new Player(playerName.getText(), PlayerType.Human);
         botName.setText(bot.getPlayerName());
+        gameManager = new GameManager(player, bot);
+
+        scorePlayer.textProperty().bind(playerScore.asString());
+        scoreBot.textProperty().bind(botScore.asString());
+
     }
 
     public void setHumanName(String userName){
@@ -55,13 +66,45 @@ public class GameViewController implements Initializable {
                 Image img = new Image(Main.class.getResource("icons/scissors.png").toExternalForm());
                 imgPlayerMove.setImage(img);
             }
-            if(move == Move.Rock){
+            else if(move == Move.Rock){
                 Image img = new Image(Main.class.getResource("icons/raise-hand.png").toExternalForm());
                 imgPlayerMove.setImage(img);
             }
-            if(move == Move.Paper){
+            else if(move == Move.Paper){
                 Image img = new Image(Main.class.getResource("icons/palm-of-hand.png").toExternalForm());
                 imgPlayerMove.setImage(img);
+            }
+            gameManager.playRound(move);
+            IGameState state = gameManager.getGameState();
+            ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
+            updateScore(results);
+        }
+    }
+
+    private void updateScore(ArrayList<Result> results) {
+        Result result = results.get(results.size() - 1);
+        if (result.getType() == ResultType.Tie){
+            setBotImage(result.getLoserMove());
+        } else if (result.getWinnerPlayer().getPlayerType() == PlayerType.Human) {
+            playerScore.set(playerScore.get() + 1);
+            setBotImage(result.getLoserMove());
+        } else if (result.getWinnerPlayer().getPlayerType() == PlayerType.AI) {
+            botScore.set(botScore.get() + 1);
+            setBotImage(result.getWinnerMove());
+        }
+    }
+
+    private void setBotImage(Move move) {
+        if(move != null) {
+            if (move == Move.Scissor) {
+                Image img = new Image(Main.class.getResource("icons/scissors.png").toExternalForm());
+                imgBotMove.setImage(img);
+            } else if (move == Move.Rock) {
+                Image img = new Image(Main.class.getResource("icons/raise-hand.png").toExternalForm());
+                imgBotMove.setImage(img);
+            } else if (move == Move.Paper) {
+                Image img = new Image(Main.class.getResource("icons/palm-of-hand.png").toExternalForm());
+                imgBotMove.setImage(img);
             }
         }
     }
