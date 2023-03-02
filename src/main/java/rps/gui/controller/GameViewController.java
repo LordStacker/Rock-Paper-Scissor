d258803b743f1275c5e3a72c49894cbc3d7b88aa
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-03-02 20:57:59 +0100

    cleaunup code

--- src/main/java/rps/gui/controller/GameViewController.java
+++ src/main/java/rps/gui/controller/GameViewController.java
@@ -3,16 +3,14 @@ package rps.gui.controller;
 // Java imports
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ObservableIntegerValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 
-import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.ResourceBundle;
 import io.github.palexdev.materialfx.controls.*;
@@ -47,7 +45,6 @@ public class GameViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         bot = new Player(getRandomBotName(), PlayerType.AI);
-        //TODO BRING THE NAME FROM FIRST FXML
         player = new Player(playerName.getText(), PlayerType.Human);
         botName.setText(bot.getPlayerName());
         gameManager = new GameManager(player, bot);
@@ -60,20 +57,10 @@ public class GameViewController implements Initializable {
     public void setHumanName(String userName){
         playerName.setText(userName);
     }
-    private void imageSelection(Move move){
+    private void doMove(Move move){
         if(move != null){
-            if(move == Move.Scissor){
-                Image img = new Image(Main.class.getResource("icons/scissors.png").toExternalForm());
-                imgPlayerMove.setImage(img);
-            }
-            else if(move == Move.Rock){
-                Image img = new Image(Main.class.getResource("icons/raise-hand.png").toExternalForm());
-                imgPlayerMove.setImage(img);
-            }
-            else if(move == Move.Paper){
-                Image img = new Image(Main.class.getResource("icons/palm-of-hand.png").toExternalForm());
-                imgPlayerMove.setImage(img);
-            }
+            // sets the image of the player move
+            setImages(move, imgPlayerMove);
             gameManager.playRound(move);
             IGameState state = gameManager.getGameState();
             ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
@@ -84,45 +71,43 @@ public class GameViewController implements Initializable {
     private void updateScore(ArrayList<Result> results) {
         Result result = results.get(results.size() - 1);
         if (result.getType() == ResultType.Tie){
-            setBotImage(result.getLoserMove());
+            setImages(result.getLoserMove(), imgBotMove);
         } else if (result.getWinnerPlayer().getPlayerType() == PlayerType.Human) {
             playerScore.set(playerScore.get() + 1);
-            setBotImage(result.getLoserMove());
+            setImages(result.getLoserMove(), imgBotMove);
         } else if (result.getWinnerPlayer().getPlayerType() == PlayerType.AI) {
             botScore.set(botScore.get() + 1);
-            setBotImage(result.getWinnerMove());
+            setImages(result.getWinnerMove(), imgBotMove);
         }
     }
 
-    private void setBotImage(Move move) {
-        if(move != null) {
-            if (move == Move.Scissor) {
-                Image img = new Image(Main.class.getResource("icons/scissors.png").toExternalForm());
-                imgBotMove.setImage(img);
-            } else if (move == Move.Rock) {
-                Image img = new Image(Main.class.getResource("icons/raise-hand.png").toExternalForm());
-                imgBotMove.setImage(img);
-            } else if (move == Move.Paper) {
-                Image img = new Image(Main.class.getResource("icons/palm-of-hand.png").toExternalForm());
-                imgBotMove.setImage(img);
-            }
+    private void setImages(Move move, ImageView imageView) {
+        if (move == Move.Scissor) {
+            Image img = new Image(Objects.requireNonNull(Main.class.getResource("icons/scissors.png")).toExternalForm());
+            imageView.setImage(img);
+        } else if (move == Move.Rock) {
+            Image img = new Image(Objects.requireNonNull(Main.class.getResource("icons/raise-hand.png")).toExternalForm());
+            imageView.setImage(img);
+        } else if (move == Move.Paper) {
+            Image img = new Image(Objects.requireNonNull(Main.class.getResource("icons/palm-of-hand.png")).toExternalForm());
+            imageView.setImage(img);
         }
     }
 
     @FXML
     private void rockSelection(ActionEvent actionEvent){
         Move selection = Move.Rock;
-        imageSelection(selection);
+        doMove(selection);
     }
     @FXML
     private void paperSelection(ActionEvent actionEvent){
         Move selection = Move.Paper;
-        imageSelection(selection);
+        doMove(selection);
     }
     @FXML
     private void scissorsSelection(ActionEvent actionEvent){
         Move selection = Move.Scissor;
-        imageSelection(selection);
+        doMove(selection);
     }
 
 
